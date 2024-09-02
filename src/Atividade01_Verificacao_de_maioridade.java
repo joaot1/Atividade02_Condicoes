890f889c42025ca1ddedeb3b932a2477aaf8de9c
joaot1 <joaotombinibortolotti@Gmail.com>
2024-09-02 12:50:56 -0300

    Atividade 01 Condições

---
+++ src/Atividade01_Verificacao_de_maioridade.java
@@ -0,0 +1,14 @@
+public class Atividade01_Verificacao_de_maioridade {
+        public static void main(String[] args) {
+
+
+            int Idade = 18;
+
+            // Verificação de MAIORIDADE.
+            if (Idade >= 18) {
+                System.out.println("A pessoa é maior de idade");
+            }else{
+                System.out.println("A pessoa é menor do idade");
+            }
+        }
+}
